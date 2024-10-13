3cee2103d1ff80754bf250c9b29324f45c060c4c
Anastasia Gegel <gegel.nastya@mail.ru>
2024-10-14 02:58:28 +0800

    Добавлено описание и руководство использования для компонента BaseBadge

--- src/components/ui/__stories__/BaseBadge.stories.ts
+++ src/components/ui/__stories__/BaseBadge.stories.ts
@@ -5,6 +5,23 @@ const meta: Meta<typeof BaseBadge> = {
   title: 'ui/BaseBadge',
   component: BaseBadge,
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+
+          BaseBadge - компонет для отображения статуса проекта.
+        Элемент состоит из контента внутри значка.
+
+        Руководство использования:
+
+        SLOTS: специальные области в компоненте, где можно разместить другой контент. Они позволяют "встраивать" контент из родительского компонента.
+
+          default: Передайте строку в качестве значения, чтобы отобразить контент элемента.
+          `,
+      },
+    },
+  },
   argTypes: {
     default: {
       type: 'string',
