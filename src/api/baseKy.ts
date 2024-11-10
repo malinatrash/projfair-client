7fb26ba0b65f761936fff0af7570e20dc83f9df8
Pavel Naumov <malinatrashh@gmail.com>
2024-11-10 18:27:37 +0800

    обновлен общий .env + Добавлена логика выбора API_URL в зависимости от парметров запуска

--- src/api/baseKy.ts
+++ src/api/baseKy.ts
@@ -10,8 +10,13 @@ const toast = useToast();
 // 1 час = 3600000 мс
 export const DEFAULT_QUERY_STALE_TIME = 2 * 3600000;
 
+const apiUrl =
+  import.meta.env.MODE !== 'dev'
+    ? import.meta.env.VITE_API_URL_LOCAL
+    : import.meta.env.VITE_API_URL;
+
 export const baseKyInstance = ky.create({
-  prefixUrl: import.meta.env.VITE_API_URL,
+  prefixUrl: apiUrl,
   retry: { limit: 5, statusCodes: [408, 413, 429, 502, 503, 504] },
   hooks: {
     beforeRequest: [
